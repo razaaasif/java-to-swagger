714f5b0746055780609cf2652be014b025d1b4ac
razaasif <aasifraza9123@gmail.com>
2023-08-21 21:48:02 +0530

    remove readme

--- src/main/java/com/raza/swaggergenerator/SwaggerYamlGeneratorApplication.java
+++ src/main/java/com/raza/swaggergenerator/SwaggerYamlGeneratorApplication.java
@@ -1,7 +1,5 @@
 package com.raza.swaggergenerator;
 
-import javax.annotation.security.RolesAllowed;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
